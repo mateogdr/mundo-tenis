f68501171b9a14d9073f65e1479e466b30d2950a
mateogdr <mateogdr.upm@gmail.com>
2026-05-13 12:38:57 +0200

    Remove personal data from seeders and footer

--- app/models/seeders/fillUsuarios.py
+++ app/models/seeders/fillUsuarios.py
@@ -3,17 +3,17 @@ from models.usuarios import Usuario
 
 def seedUsuarios():
     usuarios = [
-        {"username": "mateo_admin", "email": "mateo.garridodelrio@alumnos.upm.es", "password": "password123", "role": "admin"},
-        {"username": "rfederer", "email": "roger@atp.com", "password": "suiza_power", "role": "user"},
-        {"username": "rafa_fan", "email": "vamosrafa@gmail.com", "password": "userpass", "role": "user"},
-        {"username": "alcaraz_fan", "email": "carlosfan@gmail.com", "password": "esp_power", "role": "user"},
-        {"username": "djokovic_fan", "email": "novakfan@gmail.com", "password": "serbia123", "role": "user"},
-        {"username": "sinner_fan", "email": "jannikfan@gmail.com", "password": "italia123", "role": "user"},
-        {"username": "medvedev_fan", "email": "daniilfan@gmail.com", "password": "rusia123", "role": "user"},
-        {"username": "tsitsipas_fan", "email": "stefanosfan@gmail.com", "password": "grecia123", "role": "user"},
-        {"username": "zverev_fan", "email": "alexfan@gmail.com", "password": "alemania123", "role": "user"},
-        {"username": "fritz_fan", "email": "taylorfan@gmail.com", "password": "eeuu123", "role": "user"},
-        {"username": "tiafoe_fan", "email": "francesfan@gmail.com", "password": "eeuu456", "role": "user"}
+        {"username": "admin", "email": "admin@mundotenis.com", "password": "admin1234", "role": "admin"},
+        {"username": "rfederer", "email": "rfederer@mundotenis.com", "password": "suiza_power", "role": "user"},
+        {"username": "rafa_fan", "email": "rafa_fan@mundotenis.com", "password": "userpass", "role": "user"},
+        {"username": "alcaraz_fan", "email": "alcaraz_fan@mundotenis.com", "password": "esp_power", "role": "user"},
+        {"username": "djokovic_fan", "email": "djokovic_fan@mundotenis.com", "password": "serbia123", "role": "user"},
+        {"username": "sinner_fan", "email": "sinner_fan@mundotenis.com", "password": "italia123", "role": "user"},
+        {"username": "medvedev_fan", "email": "medvedev_fan@mundotenis.com", "password": "rusia123", "role": "user"},
+        {"username": "tsitsipas_fan", "email": "tsitsipas_fan@mundotenis.com", "password": "grecia123", "role": "user"},
+        {"username": "zverev_fan", "email": "zverev_fan@mundotenis.com", "password": "alemania123", "role": "user"},
+        {"username": "fritz_fan", "email": "fritz_fan@mundotenis.com", "password": "eeuu123", "role": "user"},
+        {"username": "tiafoe_fan", "email": "tiafoe_fan@mundotenis.com", "password": "eeuu456", "role": "user"}
     ]
 
     for u in usuarios:
